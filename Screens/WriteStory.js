92e478b3f6b8d6081d814ef9802237505cc8d26e
Adhita PL <adhitapl18@gmail.com>
2021-02-03 09:54:49 +0530

    Project Done

--- Screens/WriteStory.js
+++ Screens/WriteStory.js
@@ -1,62 +1,94 @@
 import React from 'react';
-import {Text, View, TextInput, TouchableOpacity, StyleSheet  } from 'react-native';
-import { Header } from 'react-native-elements';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert, ToastAndroid,
+ KeyboardAvoidingView} from 'react-native';
+import db from '../Config';
 
-export default class Insta extends React.Component{
-    render() {
+
+export default class WriteStoryScr extends React.Component{
+    constructor(){
+        super();
+        this.state={
+          title: '',
+          author: '',
+          story: '',
+        }
+    }
+
+    submitStory = ()=>{
+      db.collection("stories").where("ST0001" ).add({
+          title: this.state.title,
+          author: this.state.author,
+          story: this.state.story
+      })
+      this.setState({
+          title: "",
+          author: "",
+          story: ""
+      })
+      alert('Story Submitted Successfully ✅✅');
+  }
+    
+    render(){
         return(
-            <View style = {{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-                <Header
-                    backgroundColor={'#E11584'}
-                    centerComponent={{
-                        text: 'Story Hub',
-                        style: { color: '#fff', fontSize: 18 },
-                    }}
-                />
-                <TextInput style={styles.inputBox} placeholder={'Story Title'} />
-                <TextInput style={styles.inputBox}  placeholder={'Author'} />
-                <TextInput style={styles.inputBox2}  placeholder={'Write your story here'} multiline={true}/>
-                <TouchableOpacity style = {styles.submitButton} >
-                    <Text style = {styles.text}>Submit</Text>
-                </TouchableOpacity>
-            </View>
+          <KeyboardAvoidingView  style={styles.container} behavior="padding" enabled>
+            <Text style={styles.story}> STORY HUB </Text>
+          <TextInput style={styles.inputBox} placeholder={'Name of your story'} />
+          <TextInput style={styles.inputBox}  placeholder={'Author of the  story'} />
+          <TextInput style={styles.inputBox2}  placeholder={'Write your story here'} multiline={true}/>
+          <TouchableOpacity style={styles.submit} onPress={this.submitStory}>
+                      <Text style={styles.text}>Submit</Text>
+          </TouchableOpacity>
+          </KeyboardAvoidingView>
         )
     }
 }
+
 const styles = StyleSheet.create({
     inputBox: {
-        marginTop: 30,
-        marginLeft:30,
-        marginRight:30,
-        width: 330,
+        marginTop: 20,
+        width: 380,
         alignSelf: 'center',
         height: 40,
         textAlign: 'center',
         borderWidth: 4,
     },
     inputBox2: {
-        marginTop: 30,
-        marginLeft:30,
-        marginRight:30,
-        width: 330,
+        marginTop: 20,
+        width: 380,
         alignSelf: 'center',
         height: 200,
         textAlign: 'center',
         borderWidth: 4,
     },
-    submitButton: {
-        justifyContent:'center',
-        borderWidth:4,
-        backgroundColor:'#E11584',
-        marginTop:10,
-        marginLeft:650,
-        width:100,
-        height:45,
-    },
-    text: {
-        color:'blue',
+    text:{
         fontSize:20,
         textAlign: 'center',
         fontWeight: 'bold'
+    },
+    submit:{
+        justifyContent:'center',
+        borderWidth:4,
+        backgroundColor:'#ff1493',
+        marginTop:10,
+        marginLeft:130,
+        width:100,
+        height:45,
+    },
+    story:{
+        justifyContent:'center',
+        alignItems:'center',
+        textAlign: 'center',
+        backgroundColor: 'pink',
+        fontSize: 45,
+        fontWeight: 'bold',
+    },
+    head:{
+        justifyContent:'center',
+        alignItems:'center',
+        textAlign: 'center',
+        backgroundColor: 'aqua',
+        fontSize: 25,
+        fontWeight: 'bold',
     }
+    
 })
